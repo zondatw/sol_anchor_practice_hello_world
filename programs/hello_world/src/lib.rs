b93927ea1a3bef02b9e52f6c28ee91129e92ce32
zonda yang <u226699@gmail.com>
2024-02-19 23:03:14 +0800

    Add hello anchor example

--- programs/hello_world/src/lib.rs
+++ programs/hello_world/src/lib.rs
@@ -3,13 +3,31 @@ use anchor_lang::prelude::*;
 declare_id!("DfAxVwzZccekFR6LZJWSb2t46wUGwGKheh39v3cYv3yZ");
 
 #[program]
-pub mod hello_world {
+pub mod hello_anchor {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        ctx.accounts.new_account.data = data;
+        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // We must specify the space in order to initialize an account.
+    // First 8 bytes are default account discriminator,
+    // next 8 bytes come from NewAccount.data being type u64.
+    // (u64 = 64 bits unsigned integer = 8 bytes)
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct NewAccount {
+    data: u64
+}
--- tests/hello_world.ts
+++ tests/hello_world.ts
@@ -1,16 +1,61 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { HelloWorld } from "../target/types/hello_world";
+// import { HelloWorld } from "../target/types/hello_world";
+import { HelloAnchor } from "../target/types/hello_anchor";
 
-describe("hello_world", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// describe("hello_world", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
+//   const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const assert = require("assert");
+
+describe('Test', () => {
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+
+  const pg = anchor.workspace.HelloAnchor as Program<HelloAnchor>;
+
+  it('initialize', async () => {
+    // Generate keypair for the new account
+    const newAccountKp = anchor.web3.Keypair.generate();
+
+    // Send transaction
+    const data = new anchor.BN(42)
+    const txHash = await pg.methods
+      .initialize(data)
+      .accounts({
+        newAccount: newAccountKp.publicKey,
+        signer: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([newAccountKp])
+      .rpc()
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`)
+
+
+    // Confirm transaction
+    await provider.connection.confirmTransaction(txHash)
+
+
+    // Fetch the created account
+    const newAccount = await pg.account.newAccount.fetch(
+      newAccountKp.publicKey
+    )
+
+
+    console.log('On-chain data is:', newAccount.data.toString())
+
+
+    // Check whether the data on-chain is equal to local 'data'
+    assert(data.eq(newAccount.data))
+  })
+})
